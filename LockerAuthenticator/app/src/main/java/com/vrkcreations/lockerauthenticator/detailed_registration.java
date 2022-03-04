b9e94686d9212e6abd75e3073982084e89f34f57
vineethraik <vineethraik23082000@gmail.com>
2022-03-04 15:54:34 +0530

    added detailed registration activity

---
+++ LockerAuthenticator/app/src/main/java/com/vrkcreations/lockerauthenticator/detailed_registration.java
@@ -0,0 +1,14 @@
+package com.vrkcreations.lockerauthenticator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detailed_registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_registration);
+    }
+}
