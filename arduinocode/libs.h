41ececf0aee527accec589665c9a1ba9c039d786
vineethraik <vineethraik23082000@gmail.com>
2022-03-04 15:34:39 +0530

    added libs.h

---
+++ arduinocode/libs.h
@@ -0,0 +1,22 @@
+#include <SPI.h>
+
+#include <Wire.h>
+
+#include <Adafruit_SH110X.h>
+
+#include <Adafruit_GFX.h>
+
+#include <Hash.h>
+
+#include <EEPROM.h>
+
+
+
+
+#ifndef locker_h
+#include"locker.h"
+#endif
+
+#ifndef display_h
+#include"display.h"
+#endif
