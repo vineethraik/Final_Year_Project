6ba18209454d0a18a3fad569476195f1ee5c4110
vineethraik <vineethraik23082000@gmail.com>
2022-03-05 20:36:13 +0530

    added Locker class

---
+++ LockerAuthenticator/app/src/main/java/com/vrkcreations/lockerauthenticator/Locker.java
@@ -0,0 +1,15 @@
+package com.vrkcreations.lockerauthenticator;
+
+public class Locker {
+    public String Id="";
+    private String Totp_base_hash="";
+
+    public void setTotp_base_hash(String totp_base_hash) {
+        Totp_base_hash = totp_base_hash;
+    }
+    public int getTOTP(int otp,int min,int hour,int date,int month,int year){
+
+        return 124563;
+    }
+
+}
