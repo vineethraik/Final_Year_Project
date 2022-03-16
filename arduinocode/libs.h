9b6d5ce4f7017ddeff8ba889ab91f59d9a53601c
vineethraik <vineethraik23082000@gmail.com>
2022-03-17 00:40:15 +0530

    updating libs.h

--- arduinocode/libs.h
+++ arduinocode/libs.h
@@ -1,22 +1,58 @@
+
+#include <ESP8266WiFiMulti.h>
+
+#include <NTPClient.h>
+
+#include <sstream>
+
+#include<vector>
+
 #include <SPI.h>
 
 #include <Wire.h>
 
 #include <Adafruit_SH110X.h>
 
-#include <Adafruit_GFX.h>
-
 #include <Hash.h>
 
 #include <EEPROM.h>
 
+#include <ESP8266WiFi.h>
 
+#include <WiFiManager.h>
 
+#include <ESP8266mDNS.h>
+
+#include <WiFiUdp.h>
+
+#include <ArduinoOTA.h>
+
+#ifndef pcf8574_h
+#define pcf8574_h
+#include <PCF8574.h>
+#endif
+
+#ifndef RTC_h
+#include <RTC.h>
+#define RTC_h
+#endif
 
 #ifndef locker_h
 #include"locker.h"
 #endif
 
+#ifndef OTA_h
+#include"OTA.h"
+#endif 
+
 #ifndef display_h
 #include"display.h"
+#endif
+
+#ifndef bitmaps_h
+#include"bitmaps.h"
+#endif
+
+#ifndef credentials_h
+#include"credentials.h"
 #endif
