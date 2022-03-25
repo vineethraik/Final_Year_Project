70e9b33d461ce447644ea5fcb1859d7bb886d901
vineethraik <vineethraik23082000@gmail.com>
2022-03-25 23:10:28 +0530

    updating libs.h

--- arduinocode/libs.h
+++ arduinocode/libs.h
@@ -1,3 +1,7 @@
+#define AVGDELAY 2000
+
+#define display_contrast_adress 0
+#define locker_data_addr 100
 
 #include <ESP8266WiFiMulti.h>
 
@@ -5,7 +9,7 @@
 
 #include <sstream>
 
-#include<vector>
+//#include<vector>
 
 #include <SPI.h>
 
@@ -27,6 +31,8 @@
 
 #include <ArduinoOTA.h>
 
+#include <PCF8574.h>
+
 #ifndef pcf8574_h
 #define pcf8574_h
 #include <PCF8574.h>
@@ -38,21 +44,45 @@
 #endif
 
 #ifndef locker_h
+#define locker_h
 #include"locker.h"
 #endif
 
 #ifndef OTA_h
+#define OTA_h
 #include"OTA.h"
 #endif 
 
+#ifndef connectivity_h
+#define connectivity_h
+#include"connectivity.h"
+#endif
+
+#ifndef input_h
+#define input_h
+#include"input.h"
+#endif
+
+#ifndef time_h
+#define time_h
+#include"time.h"
+#endif
+
 #ifndef display_h
+#define display_h
 #include"display.h"
 #endif
 
 #ifndef bitmaps_h
+#define bitmaps_h
 #include"bitmaps.h"
 #endif
 
 #ifndef credentials_h
+#define credentials_h
 #include"credentials.h"
 #endif
+
+
+
+using namespace std;
