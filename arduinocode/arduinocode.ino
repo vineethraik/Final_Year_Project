5a6b0b05b71f8d63d3a1501b9f327567cc7e6c3e
vineethraik <vineethraik23082000@gmail.com>
2022-03-25 23:10:48 +0530

    updating base code

--- arduinocode/arduinocode.ino
+++ arduinocode/arduinocode.ino
@@ -1,46 +1,80 @@
 #define arduinoOTA
-#define basecode
-//#define experiment
+//#define basecode
+#define experiment
 //#define maincode
 //#define RTCUPDATE
+//#define memory_init
 
 #ifdef maincode
 
 #include "libs.h"
 
 
-ESP8266WiFiMulti wifiMulti;
-long reconnect_time=5000,current_time=0;
-
+void update();
+String temp;
 
 
 void setup(){
-WiFi.mode(WIFI_AP_STA);
-WiFi.softAP("Locker","");
-wifiMulti.addAP(STASSID,STAPSK);
-wifiMulti.addAP(STASSID1,STAPSK1);
-wifiMulti.addAP(STASSID2,STAPSK2);
+locker.init();
 
-disp.init(0x3c);
 disp.clear();
 disp.logo();
 disp.clear();
+
 disp.println("Waiting for wifi");
-while (wifiMulti.run(1000)!=WL_CONNECTED){}
-disp.clear();
-disp.println("Connected to\n :"+WiFi.SSID());
-disp.println("IP1:"+WiFi.localIP().toString());
-disp.println("IP2:"+WiFi.softAPIP().toString());
-ota.init();
 delay(1000);
+wifi.connect();
+delay(1000);
+wifi.printconnection();
+//ota.init();
+delay(2000);
+io.addupdatefunction(update);
 }
 
 void loop(){
   update();
+  switch(io.read_switch()){
+    case io.NILL:
+    disp.clear();
+    disp.print("no switch pressed");
+    break;
+
+    case io.OK:
+    disp.clear();
+    disp.print("OK pressed");
+    break;
+
+    case io.UP:
+    disp.clear();
+    disp.print("UP pressed");
+    break;
+
+    case io.DOWN:
+    disp.clear();
+    disp.print("DOWN pressed");
+    break;
+
+    case io.BACK:
+    disp.clear();
+    disp.print("BACK pressed");
+    break;
+  }
+  delay(1000);
+  disp.clear();
+  disp.print("pin is:");
+  temp=io.read_pin();
+  disp.print("->"+temp);
+  delay(1000);
+  disp.clear();
+  disp.print("number:");
+  temp=io.read_number();
+  disp.print("->"+temp);
+  
 }
 
 void update(){
-  wifiMulti.run(100);
+  wifi.run(100);
   ota.handle();
+  delay(1);
 }
 #endif
