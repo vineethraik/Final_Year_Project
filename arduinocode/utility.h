d233ed27268c8287e16db8041eded34192000d29
vineethraik <vineethraik23082000@gmail.com>
2022-05-01 11:30:40 +0530

    adding utility.h for arduino

---
+++ arduinocode/utility.h
@@ -0,0 +1,16 @@
+#include "libs.h"
+
+class utility{
+  private:
+  static char store[]{
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+    '0','1','2','3','4','5','6','7','8','9',
+    }
+
+  public:
+  String get_hash(String);
+  String get_pin_from_hash(String);
+  String get_rand_string(int);
+
+}utl;
