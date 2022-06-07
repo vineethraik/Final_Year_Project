7d3415daa2c799f54f41e1de6f7441f9cd6bad74
vineethraik <vineethraik23082000@gmail.com>
2022-06-08 02:22:39 +0530

    optimizing OTA

--- arduinocode/OTA.h
+++ arduinocode/OTA.h
@@ -15,25 +15,26 @@ void OTA::init(){
   ArduinoOTA.setPasswordHash("4a1e307fbe9fc730bbb117b5c4d27342");
   ArduinoOTA.onStart([]() {
     disp.clear();
-    disp.println("Started Downloading");
+    disp.println(F("Started Downloading"));
     disp.progress=true;
   });
 
   ArduinoOTA.onEnd([]() {
     disp.progress=false;
     disp.clear();
-    disp.println("Download completed");
+    disp.println(F("Download completed"));
     delay(1000);
+    disp.clear();
   });
 
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
     disp.oled.clearDisplay();
     disp.oled.setCursor(0, 0);
-    disp.oled.print("total bytes:");
+    disp.oled.print(F("total bytes:"));
     disp.oled.print(total);
-    disp.oled.print("\n\nBytes downloaded:\n");
+    disp.oled.print(F("\n\nBytes downloaded:\n"));
     disp.oled.print(progress);
-    disp.oled.print("\n\nDownloading:");
+    disp.oled.print(F("\n\nDownloading:"));
     disp.oled.print((progress*100)/total);
     disp.oled.print("\%");
     disp.oled.display();
@@ -44,15 +45,15 @@ void OTA::init(){
       disp.clear();
     disp.println("Error["+String(error)+"]: ");
     if (error == OTA_AUTH_ERROR) {
-      disp.println("Auth Failed");
+      disp.println(F("Auth Failed"));
     } else if (error == OTA_BEGIN_ERROR) {
-      disp.println("Begin Failed");
+      disp.println(F("Begin Failed"));
     } else if (error == OTA_CONNECT_ERROR) {
-      disp.println("Connect Failed");
+      disp.println(F("Connect Failed"));
     } else if (error == OTA_RECEIVE_ERROR) {
-      disp.println("Receive Failed");
+      disp.println(F("Receive Failed"));
     } else if (error == OTA_END_ERROR) {
-      disp.println("End Failed");
+      disp.println(F("End Failed"));
     }
   });
   ArduinoOTA.begin();
